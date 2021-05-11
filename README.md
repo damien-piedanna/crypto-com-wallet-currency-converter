1cf7a7ce11cbfee6888ca9b56d60a13454153506
damien-piedanna <da.piedanna@gmail.com>
2021-05-11 23:33:14 +0200

    1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# Crypto.com - Wallet currency converter
+
+Unnoficial simple extension used to convert BTC in other currency value in crypto.com wallet.
+This extension add a select to display the value of your assets in different currency.
+The bitcoin price may not be real time updated so the amount may not 100% exact.
+
+![before](other/before.png)
+![after](other/after.png)
---
+++ data/convert.js
@@ -0,0 +1,60 @@
+(function() {
+  "use strict";
+  let bitcoinprice;
+  let currency = "USD";
+  let tableLoaded = false;
+  let nbtable = 0;
+
+  $(document).ready(function() {
+    fetchBitcoinPriceAndInit();
+  });
+  
+  async function fetchBitcoinPriceAndInit() {
+    $.ajax({
+      type: "GET",
+      url: "https://blockchain.info/ticker",
+      success: function (data) {
+        console.log(data);
+        bitcoinprice = data;
+        init();
+      }
+    });
+  }
+
+  function init() {
+    let whilenotexist = setInterval(function() {
+      let rows = $('table.e-table__body > tbody > tr');
+      if (rows.length) {
+        if (!tableLoaded) {
+          updateTable(rows);
+          tableLoaded = true;
+        }
+      } else {
+        tableLoaded = false;
+      }
+    }, 500);
+  }
+
+  function updateTable(rows) {
+    nbtable++;
+    let select = $('<select id="currency-selector"><option value="USD">USD</option><option value="EUR">EUR</option><option value="GBP">GBP</option><option value="BTC">BTC</option></select>').on('change', function() {
+      currency = this.value;
+      updateRows($('table.e-table__body > tbody > tr'));
+    });
+    $('th.e-table-' + nbtable + '_column_' + ((6*nbtable)-1) + ' > .cell > .sortable').html('‏‏‎ <span class="caret-wrapper"><i class="sort-caret ascending "></i><i class="sort-caret descending "></i></span>').before(select);
+    updateRows(rows);
+  }
+
+  function updateRows(rows) {
+    rows.each(function() {
+      let column = $(this).find('.e-table-' + nbtable + '_column_' + ((6*nbtable)-1));
+      column.find('.converted-value').remove();
+      let amount = currency == "BTC" ? column.find('.cell').html() : Math.round((column.find('.cell').html() * bitcoinprice[currency]['last']) * 10) / 10;
+      let convertedCell = '<div class="cell converted-value">' + amount + '</div>';
+      column.append(convertedCell);
+      column.find('.cell').first().hide().bind('DOMSubtreeModified', function() {
+        column.find('.converted-value').html(convertedCell);
+      });
+    });
+  }
+})();
